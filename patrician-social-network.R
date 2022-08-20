9c8472f6aa79d016881e34be8516a2e1404a1488
JJ Merelo <jjmerelo@gmail.com>
2022-08-20 15:32:48 +0200

    Trying to get names right

--- patrician-social-network.R
+++ patrician-social-network.R
@@ -5,13 +5,33 @@ venice.sn <- graph.data.frame(data.frame(data$V1,data$V2),directed=F)
 plot(venice.sn)
 V(venice.sn)$degree <- degree(venice.sn)
 cnet <- cluster_edge_betweenness(venice.sn)
-plot(cnet,venice.sn)
-
-node.type <- read.csv("resources/family-positions.csv")
 
 library(Dict)
-family.types <- Dict$new()
+node.type <- read.csv("resources/family-positions.csv")
+node.type.dict <- Dict$new()
 
 for (r in 1:nrow(node.type)) {
-  family.types[node.type[r,"Family.name"])] <- print(node.type[r,"Positions"]
+  print(node.type[r,])
+  node.type.dict[ node.type[r,"Family.name"] ] <-  node.type[r,"Positions"]
 }
+
+positions <- c()
+shapes <- c()
+
+for (r in 1:nrow(V(venice.sn))) {
+  
+  print(r)
+  if (node.type.dict[r]) {
+    positions <- append(positions, node.type[r,"Positions"])
+    shapes <- append(shapes,
+                     ifelse(node.type[r,"Positions"] == "Doge",
+                            "square",
+                            ifelse(node.type(r,"Positions") == "Dogaressa","rectangle","circle")))
+    
+  } else {
+    positions <- append(positions,"None")
+    shapes <- append(shapes,"sphere")
+  }
+  
+}
+plot(cnet,venice.sn,shape=V(venice.sn)$shape)
