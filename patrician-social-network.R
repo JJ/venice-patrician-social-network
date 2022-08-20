98b0736705e71738d2ff1bf201274af20f06d50f
JJ Merelo <jjmerelo@gmail.com>
2022-08-20 14:15:49 +0200

    Starting to process better the node attributes

--- patrician-social-network.R
+++ patrician-social-network.R
@@ -6,3 +6,12 @@ plot(venice.sn)
 V(venice.sn)$degree <- degree(venice.sn)
 cnet <- cluster_edge_betweenness(venice.sn)
 plot(cnet,venice.sn)
+
+node.type <- read.csv("resources/family-positions.csv")
+
+library(Dict)
+family.types <- Dict$new()
+
+for (r in 1:nrow(node.type)) {
+  family.types[node.type[r,"Family.name"])] <- print(node.type[r,"Positions"]
+}
