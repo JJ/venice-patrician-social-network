2bb3e85a3f269c06367142ff1498910042090dac
JJ Merelo <jjmerelo@gmail.com>
2022-08-22 20:32:22 +0200

    Family data completed, closes #14
    Also plots name size proportional to number of doges

--- patrician-social-network.R
+++ patrician-social-network.R
@@ -10,22 +10,26 @@ node.type <- read.csv("resources/family-data.csv")
 node.type.dict <- list()
 
 for (r in 1:nrow(node.type)) {
-  node.type.dict[[ node.type[r,"Family.name"] ]]  <-  list( "positions"= node.type[r,"Positions"],
+  node.type.dict[[ node.type[r,"Family"] ]]  <-  list( "positions"= node.type[r,"Positions"],
                                                             "doges"= node.type[r,"Doges"],
                                                             "type" = node.type[r,"Type"] )
 }
 
 positions = NULL
 shapes = NULL
+doges = NULL
+label.colors = NULL
+label.colors.choice = c("white","lightgray","gray","lightblue","lightgreen","lightyellow", "magenta","darkgray","black")
 
 for (r in V(venice.sn)$name) {
   if (! is.null(node.type.dict[[r]])) {
-    positions <- c(positions, node.type.dict[[r]])
+    positions <- c(positions, node.type.dict[[r]]$positions)
     shapes <- c(shapes,
-                     ifelse(node.type.dict[[r]] == "Doge",
+                     ifelse(node.type.dict[[r]]$positions == "Doge",
                             "square",
-                            ifelse(node.type.dict[[r]] == "Dogaressa","sphere","circle")))
-    
+                            ifelse(node.type.dict[[r]]$positions == "Dogaressa","sphere","circle")))
+    doges <- c(1+as.numeric(node.type.dict[[r]]$doges))
+    label.colors <- c(label.colors,label.colors.choice[1+as.numeric(node.type.dict[[r]]$doges)])
   } else {
     positions <- c(positions,"None")
     shapes <- c(shapes,"sphere")
@@ -35,6 +39,7 @@ for (r in V(venice.sn)$name) {
 
 V(venice.sn)$positions <- positions
 V(venice.sn)$shapes <- shapes
+V(venice.sn)$label.colors <- label.colors
 
 plot(cnet,venice.sn,vertex.shape=V(venice.sn)$shapes)
 
@@ -42,7 +47,9 @@ V(venice.sn)$betweenness <- betweenness(venice.sn)
 
 plot(cnet,venice.sn,vertex.shape=V(venice.sn)$shapes,
      layout=layout_with_fr(venice.sn),
-     vertex.size=5+(V(venice.sn)$betweenness/20))
+     vertex.size=5+(V(venice.sn)$betweenness/20),
+     vertex.frame.color=V(venice.sn)$label.colors,
+     vertex.frame.width=)
 
 write_graph(venice.sn,"resources/venice-social-network.graphml",format="graphml")
 
