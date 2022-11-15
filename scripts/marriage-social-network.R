18ea385013e0d7edb30179d8fd6450d43b6f7a16
JJ Merelo <jjmerelo@gmail.com>
2022-11-15 12:52:01 +0100

    First analysis of the marriage social network

---
+++ scripts/marriage-social-network.R
@@ -0,0 +1,21 @@
+library(igraph)
+
+marriages <- read.csv("data/venice_marriages_puga_treffler_families.csv")
+marriages <- marriages[ marriages$husband_familyname_std != '' & marriages$wife_familyname_std != '',]
+marriages <- marriages[ marriages$husband_familyname_std != marriages$wife_familyname_std,]
+marriages.sn <- graph.data.frame(data.frame(marriages$husband_familyname_std,marriages$wife_familyname_std),directed=F)
+plot(marriages.sn)
+V(marriages.sn)$degree <- degree(marriages.sn)
+
+V(marriages.sn)$betweenness <- betweenness(marriages.sn)
+V(marriages.sn)$eigen <- eigen_centrality(marriages.sn)
+V(marriages.sn)$pr <- unname(unlist(page_rank(marriages.sn)$vector))
+
+plot(marriages.sn,
+     layout=layout_with_sugiyama(marriages.sn),
+     vertex.size=V(marriages.sn)$betweenness/500,
+     vertex.label.cex= V(marriages.sn)$degree/500,
+     vertex.label.dist=1,
+     vertex.label.degree=V(marriages.sn)$degree)
+
+
